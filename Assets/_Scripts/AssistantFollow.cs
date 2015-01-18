5efe84527aa346239723c41299fca1f262ba5fce
kellydom <kellydom@umich.edu>
2015-01-17 20:13:40 -0500

    Dialogue additions and also turtle

---
+++ Assets/_Scripts/AssistantFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssistantFollow : MonoBehaviour {
+
+	public GameObject target;
+
+	Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!GameController.S.inOverworld) return;
+
+		velocity = Vector3.Lerp (velocity, target.transform.position + Vector3.up * 1.5f + Vector3.right - transform.position, Random.Range (0.5f, 1.0f));
+
+		transform.position += velocity * Time.deltaTime * 3;
+	}
+}
---
+++ Assets/_Scripts/BattleDialogue.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BattleDialogue : MonoBehaviour {
+	public static BattleDialogue S;
+
+	public GameObject speechBubble;
+	GameObject bubbleOnScreen;
+	public GameObject doctorPos;
+	public GameObject assistantPos;
+	public GameObject patientPos;
+
+	public Image image;
+	public Text text;
+
+	bool textOnScreen;
+	bool finishText;
+	bool textFinished;
+	public bool readyForMore;
+
+	// Use this for initialization
+	void Start () {
+		if(S == null){
+			S = this;
+		}
+		else{
+			if(this != S){
+				Destroy(this.gameObject);
+			}
+		}
+		image.enabled = false;
+		text.enabled = false;
+		textOnScreen = false;
+		finishText = false;
+		textFinished = false;
+		readyForMore = true;
+	}
+
+	IEnumerator TickerTape(string speech){
+		image.enabled = true;
+		text.enabled = true;
+
+		textOnScreen = true;
+		textFinished = false;
+		finishText = false;
+		readyForMore = false;
+
+		float talkingTimer = 0;
+
+		while(talkingTimer < speech.Length && !finishText){
+			talkingTimer += Time.deltaTime;
+			int numLetters = (int)(talkingTimer * 20);
+			numLetters = Mathf.Clamp (numLetters, 0, speech.Length);
+			
+			text.text = speech.Substring(0, numLetters);
+
+			yield return 0;
+		}
+		text.text = speech;
+		textOnScreen = false;
+		textFinished = true;
+
+	}
+
+	public void SaySomething(Dialogue.Speaker speaker, string speech){
+		string speak = "";
+		Vector3 bubblePos = Vector3.zero;
+
+		if(speaker == Dialogue.Speaker.Assistant){
+			bubblePos = assistantPos.transform.position;
+			speak = "ASSISTANT: ";
+		}
+		if(speaker == Dialogue.Speaker.Doctor) {
+			bubblePos = doctorPos.transform.position;
+			speak = "PLAYER: ";
+		}
+		if(speaker == Dialogue.Speaker.Patient) {
+			bubblePos = patientPos.transform.position;
+			speak = "PATIENT: ";
+		}
+
+		bubbleOnScreen = Instantiate(speechBubble, bubblePos, Quaternion.identity) as GameObject;
+		Vector3 scale = bubbleOnScreen.transform.localScale;
+		scale *= 0.7f;
+		bubbleOnScreen.transform.localScale = scale;
+
+		Vector3 eulerAngle = bubbleOnScreen.transform.rotation.eulerAngles;
+		eulerAngle.y += 180;
+		bubbleOnScreen.transform.rotation = Quaternion.Euler (eulerAngle);
+
+		string totalSpeech = speak + speech;
+
+		StartCoroutine(TickerTape(totalSpeech));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0) && textOnScreen){
+			finishText = true;
+		}
+		if(Input.GetMouseButtonDown(0) && textFinished){
+			text.enabled = false;
+			image.enabled = false;
+			textFinished = false;
+			readyForMore = true;
+			Destroy (bubbleOnScreen);
+		}
+	}
+}
